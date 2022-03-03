017ab94d206825b36612ffbeee80434adaa3274f
Student: Peter Peter <94848956+pschondo2005@users.noreply.github.com>
2022-03-03 01:38:40 +0000

    everything

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 //Selection & Insertion Sorts
 import java.io.*;
-import static java.lang.System.*;
 import java.util.Scanner;
-import java.lang.Math;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Main{
   public static void main (String str[]) throws IOException {
@@ -143,7 +142,8 @@ int[] elements2 = {21, 17, 60, 20, 56, 12};
       }
       System.out.println();
     }
-    }int[] elements5= {21, 17, 60, 20, 56, 12};
+    } 
+ArrayList<Integer> elements5 = new ArrayList<>(Arrays.asList(21, 17, 60, 20, 56, 12));
     System.out.println("Printing Unsorted Array");
     for(int go:elements5)
       {
@@ -151,20 +151,20 @@ int[] elements2 = {21, 17, 60, 20, 56, 12};
       }
     System.out.println("\nPrinting Each Pass Through the Selection Sort");
     //Selection Sort
-    for (int j = 0; j < elements5.length -1; j++)
+    for (int j = 0; j < elements5.size() -1; j++)
     {
       int minIndex = j;
-      for (int k = j + 1; k < elements5.length; k++)
+      for (int k = j + 1; k < elements5.size(); k++)
       {
-        if (elements[k] < elements[minIndex])
+        if (elements5.get(k) < elements5.get(minIndex))
         {
           minIndex = k;
         }
       }
-      int temp = elements5[j];
-      elements5[j] = elements5[minIndex];
-      elements5[minIndex] = temp;
-    if(j<elements5.length -2)
+      int temp = elements5.get(j);
+      elements5.set(j,elements5.get(minIndex));
+      elements5.set(minIndex,elements5.get(temp));
+    if(j<elements5.size() -2)
     {
       for (int num : elements5)
       {
@@ -180,25 +180,26 @@ int[] elements2 = {21, 17, 60, 20, 56, 12};
 
 //Insertion Sort
     System.out.println("\nPrinting Unsorted Array");
-int[] elements6 = {21, 17, 60, 20, 56, 12};
-    for(int go:elements6)
+ArrayList<Integer> elements6 = new ArrayList<>(Arrays.asList(21, 17, 60, 20, 56, 12));
+for(int go:elements6)
       {
     System.out.print(go+" ");
       }  
 
     System.out.println("\nPrinting Each Pass Through the Insertion Sort");
-    for (int j = 1; j < elements6.length; j++)
+    for (int j = 1; j < elements6.size(); j++)
     {
-      int temp = elements6[j];
+      int temp = elements6.get(j);
       int possibleIndex = j;
-      while (possibleIndex > 0 && temp < elements6[possibleIndex - 1])
+      while (possibleIndex > 0 && temp < elements6.get
+(possibleIndex - 1))
       {
-        elements6[possibleIndex] = elements6[possibleIndex - 1];
+        elements6.set(possibleIndex, elements6.get(possibleIndex - 1));
         possibleIndex--;
         
       }
-      elements6[possibleIndex] = temp;
-      if(j<elements6.length -1)
+      elements6.set(possibleIndex, temp);
+      if(j<elements6.size() -1)
     {
       for (int num : elements6)
       {
@@ -214,64 +215,28 @@ int[] elements6 = {21, 17, 60, 20, 56, 12};
     }
     System.out.println();
     //alphabetical sorting
-    String[] elements6 ={"bus","truck","van","semi","car"};
-    System.out.println("Printing Unsorted Array of Strings");
-    for(String go:elements6)
-      {
-    System.out.print(go+" ");
-      }
-    System.out.println("\nPrinting Each Pass Through the Selection Sort");
-    //Selection Sort
-    for (int j = 0; j < elements6.length -1; j++)
-    {
-      int minIndex = j;
-      for (int k = j + 1; k < elements6.length; k++)
-      {
-     if (elements6[k].compareTo(elements6[minIndex])<0)
-        {
-          minIndex = k;
-        }
-      }
-      String temp = (elements6[j]);
-      elements6[j] = elements6[minIndex];
-      elements6[minIndex] = temp;
-    if(j<elements6.length -2)
-    {
-      for (String num : elements6)
-      {
-      System.out.print(num+" ");
-      }
-      System.out.println();
-    }
-    }
-    for (String num : elements6)
-    {
-      System.out.print(num+" ");
-    }
-
-
-   String[] elements7 = { "bus", "truck", "van", "semi","car"};
-    System.out.println("\nPrinting Unsorted Array");
+/*ArrayList<String> elements7 = new ArrayList<>(Arrays.asList("bus", "truck", "van", "semi","car"));   
+System.out.println("Printing Unsorted Array of Strings");
     for(String go:elements7)
       {
     System.out.print(go+" ");
       }
     System.out.println("\nPrinting Each Pass Through the Selection Sort");
     //Selection Sort
-    for (int j = 0; j < elements7.length -1; j++)
+    for (int j = 0; j < elements7.size() -1; j++)
     {
       int minIndex = j;
-      for (int k = j + 1; k < elements7.length; k++)
+      for (int k = j + 1; k < elements7.size(); k++)
       {
-        if (elements7[k].compareTo(elements7[minIndex])<0)
+     if (elements7.get(k).compareTo(elements7.get(minIndex))<0)
         {
           minIndex = k;
         }
       }
-      String temp = elements7[j];
-      elements7[j] = elements7[minIndex];
-      elements7[minIndex] = temp;
-    if(j<elements.length -2)
+      String temp = (elements7.get(j));
+      elements7.set(j, elements7.get(minIndex));
+      elements7.set(minIndex,temp);
+    if(j<elements7.size() -2)
     {
       for (String num : elements7)
       {
@@ -280,6 +245,42 @@ int[] elements6 = {21, 17, 60, 20, 56, 12};
       System.out.println();
     }
     }
-    
+    for (String num : elements7)
+    {
+      System.out.print(num+" ");
+    }
+
+
+   String[] elements8 = { "bus", "truck", "van", "semi","car"};
+    System.out.println("\nPrinting Unsorted Array");
+    for(String go:elements8)
+      {
+    System.out.print(go+" ");
+      }
+    System.out.println("\nPrinting Each Pass Through the Selection Sort");
+    //Selection Sort
+    for (int j = 0; j < elements8.length -1; j++)
+    {
+      int minIndex = j;
+      for (int k = j + 1; k < elements8.length; k++)
+      {
+        if (elements8[k].compareTo(elements8[minIndex])<0)
+        {
+          minIndex = k;
+        }
+      }
+      String temp = elements8[j];
+      elements8[j] = elements8[minIndex];
+      elements8[minIndex] = temp;
+    if(j<elements.length -2)
+    {
+      for (String num : elements8)
+      {
+      System.out.print(num+" ");
+      }
+      System.out.println();
+    }
+    }
+    */
   }
 }
